35e57316d564c7e72241be0dc376f2efd7759271
kurog1ri <kosenko.yosiki@gmail.com>
2024-04-05 17:07:13 +0300

    Initial Commit

---
+++ AQAppForMyThesis/AQAppForMyThesisApp.swift
@@ -0,0 +1,17 @@
+//
+//  AQAppForMyThesisApp.swift
+//  AQAppForMyThesis
+//
+//  Created by   Kosenko Mykola on 05.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AQAppForMyThesisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
